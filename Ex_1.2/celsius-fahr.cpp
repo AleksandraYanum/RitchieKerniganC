0005aef5669b55bbaa2a76f284cb81a589ef39e3
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-04-23 22:37:11 +0300

    Ex_1.2 was added.

---
+++ Ex_1.2/celsius-fahr.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* print C and F temperature (C = from 0 to 50 with step=10)*/
+int main()
+{
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 50;
+    celsius = lower;
+    step = 10;
+
+    printf("Celsius-Fahreinheit temprature\n\n");
+    while (celsius <= upper)
+    {
+        fahr = 9 * (celsius + 32) / 5;
+        printf("\t%d\t%d\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
