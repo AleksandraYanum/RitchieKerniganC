dc557630b4ca7106ad75886345ae65caaf86f3da
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-04-25 02:07:29 +0300

    Ex_1.10 was created (replace symbols with other).

---
+++ Ex_1.10/Ex_1.10.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* replace tab with "\t", space with "\b", backslash with "\\" */
+
+int main()
+{
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            printf("\\t");
+        }
+        else if (c == ' ')
+            {
+                printf("\\b");
+            }
+        else if (c == '\\')
+            {
+            printf("\\\\");
+            }
+        else putchar(c);
+    }
+    return 0;
+}
+
+
