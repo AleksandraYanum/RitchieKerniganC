9a048912d5ae98fefa3969a74949283ccec7ac66
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-04-24 23:16:30 +0300

    Ex_1.9 was created.

--- Ex_1.9/Ex_1.9.cpp
+++ Ex_1.9/Ex_1.9.cpp
@@ -2,6 +2,29 @@
 
 int main()
 {
+	int prev, c;
+	prev = 0;
+	while ((c = getchar()) != EOF)
+	{
+		if (c == ' ')
+		{
+			if (prev != ' ')
+			{
+				putchar(c);
+				prev = c;
+			}
+			else
+			{
+				prev = c;
+			}
+		}
+		else
+		{
+			putchar(c);
+			prev = c;
+		}
+		
+	}
 	return 0;
 }
 
