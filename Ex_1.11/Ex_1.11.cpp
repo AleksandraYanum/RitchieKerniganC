63fe7148fe906e0729402ac5a78768e86fd9e8eb
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-04-25 13:02:29 +0300

    Words count implemented.

--- Ex_1.11/Ex_1.11.cpp
+++ Ex_1.11/Ex_1.11.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+# define IN 1 /* in the word*/
+# define OUT 2 /* out of the word*/
+
 /* count lines, words and symbols */
 
 int main()
 {
 	int nl, nw, nc, state, c;
 	nl = nw = nc = 0;
+	state = OUT;
 	while ((c = getchar()) != EOF)
 	{
 		nc++;
@@ -13,8 +17,18 @@ int main()
 		{
 			nl++;
 		}
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			state = OUT;
+		}
+		else if (state == OUT)
+		{
+			state = IN;
+			nw++;
+		}
 	}
 	printf ("Symbols count = %d\n", nc);
 	printf("Lines count = %d\n", nl);
+	printf("Words count = %d\n", nw);
 	return 0;
 }
