c14bef9581f4728304f2b5ab1d52dd72c7012460
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-04-28 12:16:42 +0300

    Ex_1.13: array was added.

--- Ex_1.13/Ex_1.13.cpp
+++ Ex_1.13/Ex_1.13.cpp
@@ -2,20 +2,26 @@
 
 # define IN 1 /* in the word*/
 # define OUT 2 /* out of the word*/
+# define MAXSIZE 10 /* max length of input words*/
 
 /* print a histogram of the lengths of input words */
 
 int main()
 {
-	int c, nc, state;
+	int i, c, nc, state;
 	state = OUT;
 	nc = 0;
+	int hist[MAXSIZE];
+	for (i = 0; i < MAXSIZE; i++)
+	{
+		hist[i] = 0;
+	}
 	while ((c = getchar()) != EOF)
 	{
 		if ((c == ' ' || c == '\t' || c == '\n') && (state == IN))
 		{
 			state = OUT;
-			/* склдываем nc в массив*/
+			hist[nc - 1]++;			
 			printf("Word length = %d\n", nc);
 			nc = 0;
 		}
