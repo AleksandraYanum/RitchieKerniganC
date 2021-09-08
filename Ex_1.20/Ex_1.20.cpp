8e9e9674ea590b999a9bcaea40e44bca74ea8dd8
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-09-08 16:09:20 +0300

    Solution to 1.20 was added.

--- Ex_1.20/Ex_1.20.cpp
+++ Ex_1.20/Ex_1.20.cpp
@@ -1,8 +1,47 @@
 #include <stdio.h>
 
-/*  */
+/* detap: print certain amount os cpaces insted tabs*/
+
+void print_space(int count);
 
 int main()
 {
+	int len_tab = 8; /* length of tab (8 by default) */
+	int i = 0; /* position of current symbol */
+	int r; /* remainder of division */
+	int c, n;
+	i = n = r = 0;
+	while ((c = getchar()) != EOF)
+	{	
+		if (c == '\t')
+		{
+			r = i % len_tab;
+			n = len_tab - r;
+			i = i + n;
+			print_space(n);
+		}
+		else
+		{
+			putchar(c);
+			if (c == '\n')
+			{
+				i = 0;
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
 	return 0;
+}
+
+
+void print_space(int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		putchar(' ');
+	}
+	return;
 }
