e81c271a825279910bb52e03f356a90d5c24fbff
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-09-18 15:00:22 +0300

    Functions were added.

--- Ex_1.21/Ex_1.21.cpp
+++ Ex_1.21/Ex_1.21.cpp
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
 # define IN 1 /* in the word*/
-# define OUT 2 /* out of the word*/
+# define OUT 0 /* out of the word*/
 
 /* entab: prints certain amount of tabs insted spaces  */
+
+void print_tab_space (int tab_to_print, int space_to_print);
+void print_symbol(int c, int n);
 
 int main()
 {
 	int len_tab = 8; /* length of tab (8 by default) */
 	int i = 0; /* position of current symbol */
-	int c, space_count, state;
+	int c, space_count, state, tab_to_print, r, space_to_print;
 	space_count = 0;
+	tab_to_print = 0;
+	space_to_print = 0;
+	r = 0;
 	state = OUT;
 	while ((c = getchar()) != EOF)
 	{
@@ -26,13 +32,33 @@ int main()
 		{
 			if (state == OUT)
 			{
-				/* change spaces into tabs */
 				state = IN;
+				/* change spaces into tabs */
+				tab_to_print = space_count / len_tab;
+				space_to_print = space_count % len_tab;
+			
+				print_tab_space(tab_to_print, space_to_print);
 				space_count = 0;
 			}
+			
 			putchar(c);
 		}
 	}
 	
 	return 0;
+}
+void print_tab_space(int tab_to_print, int space_to_print)
+{
+	print_symbol('\t', tab_to_print);
+	print_symbol(' ', space_to_print);
+	return;
+}
+
+void print_symbol(int c, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		putchar(c);
+	}
+	return;
 }
