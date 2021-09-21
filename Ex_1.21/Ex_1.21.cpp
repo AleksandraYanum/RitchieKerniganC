b29835f025f5faa191b87c3e9f6aaec238c38e29
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-09-21 13:03:32 +0300

    Refactoring. Logic was changed

--- Ex_1.21/Ex_1.21.cpp
+++ Ex_1.21/Ex_1.21.cpp
@@ -1,43 +1,51 @@
 #include <stdio.h>
 
-# define IN 1 /* in the word*/
-# define OUT 0 /* out of the word*/
+# define IN 1 // in the word
+# define OUT 0 // out of the word
 
-/* entab: prints certain amount of tabs instead of spaces  */
+// entab: prints certain amount of tabs instead of spaces
 
 void print_tab_space (int tab_to_print, int space_to_print);
 void print_symbol(int c, int n);
 
 int main()
 {
-	int len_tab = 8; /* length of tab (8 by default) */
-	// int i = 0; /* position of current symbol */
-	int c, space_count, state, tab_to_print, space_to_print;
-	space_count = 0;
-	tab_to_print = 0;
-	space_to_print = 0;
+	int len_tab = 8; // length of tab (8 by default)
+	int state, tab_to_print, space_to_print;
+	int pos = 0; // position of current symbol;
+	int first = 0; // position of the first letter of the next word (after spaces)
+	int last = 0; // position of the last letter of the previous word (before spaces)
+	int past_tabs, all_tabs;
+	char c; // input
+	past_tabs = all_tabs = 0;
+	tab_to_print = space_to_print = 0;
 	state = OUT;
 	while ((c = getchar()) != EOF)
 	{
+		pos++;
 		if (c == ' ')
 		{
 			if (state == IN)
 			{
+				last = pos;
 				state = OUT;
 			}
-			space_count++;
+			
 		}
 		else
 		{
 			if (state == OUT)
 			{
 				state = IN;
-				/* change spaces into tabs */
-				tab_to_print = space_count / len_tab;
-				space_to_print = space_count % len_tab;
-			
+				first = pos;
+				// change spaces into tabs:
+				past_tabs = last / len_tab; // how many tab positions have we already passed
+				all_tabs = first / len_tab; // how many tab positions are there from the line beginning
+				tab_to_print = all_tabs - past_tabs;
+				space_to_print = first % len_tab;
 				print_tab_space(tab_to_print, space_to_print);
-				space_count = 0;
+				first = last = 0;
+				
 			}
 			
 			putchar(c);
