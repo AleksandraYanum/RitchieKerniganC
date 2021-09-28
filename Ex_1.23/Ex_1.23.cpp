eec5070af14d36f0f1a6a408e7e34f7dee0dfb0d
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-09-28 15:46:55 +0300

    Add logic to ignore comments in strigs (string begins from " and ends with ")

--- Ex_1.23/Ex_1.23.cpp
+++ Ex_1.23/Ex_1.23.cpp
@@ -12,6 +12,7 @@
 
 int one_line_comment = OUT; // IN or OUT (of) the one-line comment (begins with '//')
 int multi_line_comment = OUT; // IN or OUT (of) the multi-line comment (begins with '/*' and ends with '*/')
+int str = OUT; // IN or OUT (of) the printed string
 int slash_count = 0; // count of slashes that are standing in a ROW
 char prev_symb = 0; //previous symbol
 char c; // input
@@ -25,6 +26,7 @@ void slash_handler();
 void enter_handler();
 void asterisk_handler();
 void default_handler();
+void double_quote_handler();
 
 // ###########################################################################
 
@@ -45,6 +47,11 @@ int main()
 		else if (c == '*')
 		{
 			asterisk_handler();
+		}
+
+		else if (c == '"')
+		{
+			double_quote_handler();
 		}
 
 		else //other symbols
@@ -70,31 +77,45 @@ int out_of_comment()
 
 void slash_handler()
 {
-	if (out_of_comment())
+	if (str == OUT)
 	{
-		slash_count++;
-		if (slash_count == SINGLE_COMMENT_SLASH_COUNT)
+		if (out_of_comment())
 		{
-			one_line_comment = IN;
+			slash_count++;
+			if (slash_count == SINGLE_COMMENT_SLASH_COUNT)
+			{
+				one_line_comment = IN;
+			}
+		}
+		else if ((multi_line_comment == IN) && (prev_symb == '*'))
+		{
+			multi_line_comment = OUT;
 		}
 	}
-	else if ((multi_line_comment == IN) && (prev_symb == '*'))
+	else
 	{
-		multi_line_comment = OUT;
+		putchar(c);
 	}
 	return;
 }
 
 void enter_handler()
 {
-	if (out_of_comment())
+	if (str == OUT)
 	{
-		putchar(c);
-		slash_count = 0;
+		if (out_of_comment())
+		{
+			putchar(c);
+			slash_count = 0;
+		}
+		else if (one_line_comment == IN)
+		{
+			one_line_comment = OUT;
+			putchar(c);
+		}
 	}
-	else if (one_line_comment == IN)
+	else
 	{
-		one_line_comment = OUT;
 		putchar(c);
 	}
 	return;
@@ -102,33 +123,63 @@ void enter_handler()
 
 void asterisk_handler()
 {
-	if (out_of_comment())
+	if (str == OUT)
 	{
-		if (prev_symb == '/')
+		if (out_of_comment())
 		{
-			multi_line_comment = IN;
+			if (prev_symb == '/')
+			{
+				multi_line_comment = IN;
+			}
+			else
+			{
+				putchar(c);
+			}
 		}
-		else
-		{
-			putchar(c);
-		}
+	}
+	else
+	{
+		putchar(c);
 	}
 	return;
 }
 
 void default_handler()
 {
+	if (str == OUT)
+	{
+		if (out_of_comment())
+		{
+			if (slash_count < SINGLE_COMMENT_SLASH_COUNT)
+			{
+				for (int i = 0; i < slash_count; i++)
+				{
+					putchar('/');
+				}
+			}
+			putchar(c);
+			slash_count = 0;
+		}
+	}
+	else
+	{
+		putchar(c);
+	}
+	return;
+}
+
+void double_quote_handler()
+{
 	if (out_of_comment())
 	{
-		if (slash_count < SINGLE_COMMENT_SLASH_COUNT)
+		if (str == OUT)
 		{
-			for (int i = 0; i < slash_count; i++)
-			{
-				putchar('/');
-			}
+			str = IN;
 		}
-		putchar(c);
-		slash_count = 0;
+		else
+		{
+			str = OUT;
+		}
 	}
 	return;
 }
