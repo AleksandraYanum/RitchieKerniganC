f6101c7b10794f272ff849ddafdf7c7c8e7b88e3
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-10-11 13:30:55 +0300

    Create program for one type of brackets Add test and test_cases files

--- Ex_1.24/Ex_1.24.cpp
+++ Ex_1.24/Ex_1.24.cpp
@@ -3,7 +3,35 @@
 
 /* program checks a C program for syntax errors like unbalanced brackets */
 
+# define OPENING_BRACKET '{'
+# define CLOSING_BRACKET '}'
+
+
 int main()
 {
+	int bracket_count = 0; // count paired brackets: +1 if opening braket; -1 if closing bracket
+	char c; //input
+
+	while (((c = getchar()) != EOF) && (bracket_count >= 0))
+	{
+		if (c == OPENING_BRACKET)
+		{
+			bracket_count++;
+		}
+		else if (c == CLOSING_BRACKET)
+		{
+			bracket_count--;
+		}
+	}
+
+	if (bracket_count == 0)
+	{
+		printf("Program compiled");
+	}
+	else
+	{
+		printf("Program has syntax errors withn unbalanced brackets");
+	}
+
 	return EXIT_SUCCESS;
 }
