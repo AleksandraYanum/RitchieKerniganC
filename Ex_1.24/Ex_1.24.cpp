fd40195328e5b50553c4a6dbfa932b13747b8009
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-10-12 12:34:06 +0300

    Implement generic approach (for different types of brackets) Add initial version for {} brackets

--- Ex_1.24/Ex_1.24.cpp
+++ Ex_1.24/Ex_1.24.cpp
@@ -6,33 +6,51 @@
 
 # define OPENING_BRACKET '{'
 # define CLOSING_BRACKET '}'
+# define MAXSIZE 10 //max size of the array with opening brackets
 
 
 int main()
 {
-	int bracket_count = 0; // count paired brackets: +1 if opening braket; -1 if closing bracket
+	//int bracket_count = 0; // count paired brackets: +1 if opening braket; -1 if closing bracket
 	char c; //input
+	char bracket[MAXSIZE];
+	int pos = 0; //position of array elements
+	int is_error = 0; // 1 - error, 0 - not error
 
-	while (((c = getchar()) != EOF) && (bracket_count >= 0))
+	for (int i = 0; i < MAXSIZE; i++)
 	{
-		if (c == OPENING_BRACKET)
+		bracket[i] = 0;
+	}
+
+	while (((c = getchar()) != EOF) && (is_error == 0))
+	{
+		if (c == '{')							//|| (c == '[') || (c == '('))
 		{
-			bracket_count++;
+			bracket[pos] = c;
+			pos++;
 		}
-		else if (c == CLOSING_BRACKET)
+
+		else if (c == '}')
 		{
-			bracket_count--;
+			if ((pos > 0) && (bracket[pos - 1] == '{'))
+			{
+				bracket[pos - 1] = 0;
+				pos--;
+			}
+			else
+			{
+				is_error = 1;
+			}
 		}
 	}
 
-	if (bracket_count == 0)
+	if ((is_error == 0) && (pos == 0))
 	{
-		printf("Program compiled");
+		printf("Program is correct");
 	}
 	else
 	{
-		printf("Program has syntax errors withn unbalanced brackets");
+		printf("Program has syntax errors with unbalanced brackets");
 	}
-
 	return EXIT_SUCCESS;
 }
