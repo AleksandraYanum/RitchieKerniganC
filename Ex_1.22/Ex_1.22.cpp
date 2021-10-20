b0713204e1a8a675ebf9742d17fd661001c37555
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-10-20 15:12:55 +0300

    Create function print_array()

--- Ex_1.22/Ex_1.22.cpp
+++ Ex_1.22/Ex_1.22.cpp
@@ -5,11 +5,22 @@
 
 # define MAXSIZE 6 //max size of the symbol array; it's also the length of line
 
+// ###########################################################################
+// global variables
+// ###########################################################################
+
+int pos = 0; // position of current symbol; shows, how many symbols were inputed
+char symb_arr[MAXSIZE];
+
+// ###########################################################################
+// functions
+// ###########################################################################
+
+void print_array();
+
 int main()
 {
-	int pos = 0; // position of the next symbol; shows, how many symbols were inputed
 	int space_pos = 0; // position of the last space
-	char symb_arr[MAXSIZE];
 	char c;
 
 	for (int i = 0; i < MAXSIZE; i++)
@@ -32,27 +43,24 @@ int main()
 		else if (pos == MAXSIZE)
 		{
 			symb_arr[space_pos] = '\n';
-
-			for (int i = 0; i < pos; i++)
-			{
-				putchar(symb_arr[i]);
-				symb_arr[i] = 0;
-			}
-
+			print_array();
 			pos = 1;
 			symb_arr[0] = c;
 			space_pos = 0;
 		}
-
 	}
-	
+
+	print_array();
+	return EXIT_SUCCESS;
+}
+
+
+void print_array()
+{
 	for (int i = 0; i < pos; i++)
 	{
 		putchar(symb_arr[i]);
 		symb_arr[i] = 0;
 	}
-
-
-
-	return EXIT_SUCCESS;
+	return;
 }
