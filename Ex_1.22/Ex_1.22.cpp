b5819a0eeca1e8813dc40ddbe4c4afebe2893056
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-10-23 15:46:51 +0300

    Change logic with var pos, delete var pos_arr

--- Ex_1.22/Ex_1.22.cpp
+++ Ex_1.22/Ex_1.22.cpp
@@ -8,23 +8,28 @@ NB! Program accept words for input no longer than MAXSIZE letters
 */
 
 # define MAXSIZE 6 //max size of the symbol array; it's also the length of line
+# define YES 1
+# define NO 0
+
 
 // ###########################################################################
 // global variables
 // ###########################################################################
 
-int output_pos = 0; // position of current symbol in current output; shows, how many symbols were inputed
-int pos_arr = 0; //position of current symbol in array
-int space_pos = -1; // position of the last space in the array
 char symb_arr[MAXSIZE + 1];
+int pos = 0; // position of current symbol in current output; shows, how many symbols were inputed
+int space_pos = -1; // position of the last space in the array
+int array_is_empty = YES;
+
 
 // ###########################################################################
 // functions
 // ###########################################################################
 
 void print_array();
-void init_array();
-void replace_space_with_enter_in_array();
+void init_array(int index_from, int index_to);
+void array_left_shift();
+
 
 // ###########################################################################
 
@@ -32,70 +37,104 @@ int main()
 {
 	char c;
 
-	init_array();
+	init_array(0, MAXSIZE + 1);
 
 	while ((c = getchar()) != EOF)
 	{
-		if (output_pos < MAXSIZE + 1)
+		if (pos < MAXSIZE + 1)
 		{
 			if (c == ' ')
 			{
-				space_pos = pos_arr;
+				space_pos = pos;
 			}
-			symb_arr[pos_arr] = c;
-			output_pos++;
-			pos_arr++;
+			symb_arr[pos] = c;
+			pos++;
+			array_is_empty = NO;
 		}
 
-		else if (output_pos == MAXSIZE + 1)
+		else if (pos == MAXSIZE + 1)
 		{
-			replace_space_with_enter_in_array();
 			print_array();
-			symb_arr[0] = c;
-			pos_arr = 1;
-			output_pos = output_pos - space_pos;
+			putchar('\n');
+			if (array_is_empty == NO)
+			{
+				array_left_shift();
+				int last_symb = pos;
+				pos = pos - (space_pos + 1);
+				init_array(pos, last_symb);
+			}
+			else
+			{
+				pos = 0;
+			}
+			symb_arr[pos] = c;
 			if (c == ' ')
 			{
-				space_pos = 0;
+				space_pos = pos;
 			}
 			else
 			{
 				space_pos = -1;
 			}
+			pos++;
 		}
 	}
 
-	replace_space_with_enter_in_array();
 	print_array();
+	
+	if (array_is_empty == NO)
+	{
+		putchar('\n');
+		array_left_shift();
+		int last_symb = pos;
+		pos = pos - (space_pos + 1);
+		space_pos = -1;
+		init_array(pos, last_symb);
+		print_array();
+	}
+	
 	return EXIT_SUCCESS;
 }
 
 
 void print_array()
 {
-
-	for (int i = 0; i < pos_arr; i++)
+	if (space_pos > -1)
 	{
-		putchar(symb_arr[i]);
+		for (int i = 0; i <= space_pos; i++)
+		{
+			putchar(symb_arr[i]);
+		}
+	}
+	else if (space_pos == -1)
+	{
+		for (int i = 0; i < pos; i++)
+		{
+			putchar(symb_arr[i]);
+			symb_arr[i] = '\0';
+			
+		}
+		array_is_empty = YES;
+	}
+	
+	return;
+}
+
+void init_array(int index_from, int index_to)
+{
+	for (int i = index_from; i < index_to; i++)
+	{
 		symb_arr[i] = 0;
 	}
 	return;
 }
 
-void init_array()
+void array_left_shift()
 {
-	for (int i = 0; i < MAXSIZE + 1; i++)
+	for (int i = space_pos + 1; i < pos; i++)
 	{
-		symb_arr[i] = 0;
-	}
-	return;
-}
+		symb_arr[i - (space_pos + 1)] = symb_arr[i];
 
-void replace_space_with_enter_in_array()
-{
-	if (space_pos >= 0)
-	{
-		symb_arr[space_pos] = '\n';
 	}
 	return;
 }
