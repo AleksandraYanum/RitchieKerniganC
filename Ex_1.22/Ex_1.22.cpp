f211256a5298da9d5bba37ef9fa5a153664a5e84
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2021-10-27 10:12:34 +0300

    1.22: Full word wrap (#8)
    * Create initial template
    
    * Add initial logic (with not right output!)
    
    * Fix logic to save the first letter of the next word that shoul be shifted
    
    * Fix logic for EOF edge case
    
    MAXSIZE = 6
    Input: 12345 12345
    Output: 12345
                  12345
    
    * Create function print_array()
    
    * Add var pos_arr (shows symb positio in array)
    
    * Refactoring
    
    * Fix enter handling
    
    * Create function replace_space_with_enter_in_array()
    
    * Change logic with var pos, delete var pos_arr
    
    * Modify function print_array(print_to), add find_limit_to_print()
    
    * Refactoring
    
    * Add function is_delimiter()
    
    * Modify function is_delimiter(char c) - add parameter
    
    * Refactoring
    
    * Add string array for delimiters
    
    * Add all delimiter characters
    
    * Add logic to have input string not longer than MAXSIZE
    
    * Bag fix for words no longer than MAXSIZE
    
    * Refactoring

---
+++ Ex_1.22/Ex_1.22.cpp
@@ -0,0 +1,129 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+program "folds" long input lines into shorter lines 
+Version_1: Program find the right position to shift words (after delimiter character)
+NB! Program accept words for input no longer than MAXSIZE letters
+*/
+
+# define MAXSIZE 50 //max size of the symbol array; it's also the length of line
+
+// ###########################################################################
+// global variables
+// ###########################################################################
+
+char c;
+char symb_arr[MAXSIZE + 1];
+int pos = 0; // position of the next symbol in input
+int delimiter_pos = -1; // position after the last delimiter
+int print_to = -1; //the element to print array to
+char delimiter_arr[] = " /\\()\"\'-.,:;<>~!@#$%^&*|+=[]{}~\?│";
+
+// ###########################################################################
+// functions
+// ###########################################################################
+
+void print_array(int print_to, int is_new_line); //is_new_line: 1 - to print enter, 0 - not to print
+void find_limit_to_print();
+void init_array(int index_from, int index_to);
+void array_left_shift();
+int is_delimiter(char c);
+
+// ###########################################################################
+
+int main()
+{
+	init_array(0, MAXSIZE + 1);
+
+	while ((c = getchar()) != EOF)
+	{
+		if (pos < MAXSIZE)
+		{
+			
+			symb_arr[pos] = c;
+			pos++;
+			if (is_delimiter(c))
+			{
+				delimiter_pos = pos;
+			}
+			if (c == '\n')
+			{
+				print_array(pos, 0);
+				pos = 0;
+				delimiter_pos = -1;
+			}
+		}
+
+		else if (pos == MAXSIZE)
+		{
+			find_limit_to_print();
+			print_array(print_to, 1);
+			array_left_shift();
+			int last_symb = pos;
+			pos = (delimiter_pos > -1) ? (pos - delimiter_pos) : 0;
+			init_array(pos, last_symb);
+			if (c != '\n')
+			{
+				symb_arr[pos] = c;
+			}
+			pos++;
+			delimiter_pos = is_delimiter(c) ? pos : -1;
+		}
+	}
+
+	print_array(pos, 0);
+	
+	return EXIT_SUCCESS;
+}
+
+int is_delimiter(char c)
+{
+	int result = 0;
+	for (int i = 0; (delimiter_arr[i] != '\0') && (result == 0); i++)
+	{
+		if (c == delimiter_arr[i])
+		{
+			result = 1;
+		}
+	}
+	return result;
+}
+
+void find_limit_to_print()
+{
+	print_to = (delimiter_pos > -1) ? delimiter_pos : pos;
+	return;
+}
+
+void print_array(int print_to, int is_new_line)
+{
+	for (int i = 0; i < print_to; i++)
+	{
+		putchar(symb_arr[i]);
+	}
+	if (is_new_line == 1)
+	{
+		putchar('\n');
+	}
+	return;
+}
+
+void init_array(int index_from, int index_to)
+{
+	for (int i = index_from; i < index_to; i++)
+	{
+		symb_arr[i] = 0;
+	}
+	return;
+}
+
+void array_left_shift()
+{
+	for (int i = delimiter_pos; i < pos; i++)
+	{
+		symb_arr[i - delimiter_pos] = symb_arr[i];
+
+	}
+	return;
+}
