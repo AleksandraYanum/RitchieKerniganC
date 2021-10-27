a24fea581a958180b86e50821eb549ee08029eb0
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-10-27 12:26:51 +0300

    Create test project for word wrapping

---
+++ Word_wrap/Word_wrap.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+function takes the word from the global array XXX and limit_position to wrap the word
+Returns the top right possible position of word wrap, otherwise -1
+Rules:
+1. Do not leave just one letter on the line (have at least 2 letters on the line)
+2. Each part should contain vowel letter (a, e, u, etc)
+3. Wrapping ends with vowel letter (e.g. hello -> he--llo)
+*/
+
+// ###########################################################################
+// functions
+// ###########################################################################
+
+int find_word_wrap_position(int limit_position);
+
+// ###########################################################################
+
+int main()
+{
+	
+	return EXIT_SUCCESS;
+}
+
+
+int find_word_wrap_position(int limit_position)
+{
+	return -1;
+}
