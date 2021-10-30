cba4676feaf479ca4fb9cdcb405844a1823c9c40
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2021-10-30 16:42:36 +0300

    1.22: Word part wrap (#11)
    * Initial commit
    
    * Add API of new function find_word_wrap_position(int limit_position)
    
    * Fix error in main_template
    
    * Refactoring
    
    * Create test project for word wrapping
    
    * Add wrap_test function
    
    * Comment out sample code
    
    * Create function find_word_length()
    
    * Create function is_vowel()
    
    * Create function find_vowel_position()
    
    * Create function find_word_wrap_position()
    
    * Create function wrap_test()
    
    * Add logic to find 2nd vowel position
    
    * Add param to function find_vowel_position() to find the appropriate vowel (1st, 2nd, etc)
    
    * Add test case with "aleksandra"
    
    * Fix index calculation for first letter
    
    * Add check to find pos to wrap if there are no vowels or 2nd vowel is the first letter
    
    * Fix var's name
    
    * Add edge case: no nessesary vowel in word
    
    ex: "bbba" 2nd vowel is -1
    
    * Add test case template
    
    * Add 11 test cases
    
    * Comment out debug printf
    
    * Fix edge case for "aaaa" (find 3d vowel)
    
    * Fix code style
    
    * Add function print_test_result()
    
    * Fix API of find_wrap_position()
    
    * Use stub find_wrap_position()
    
    * Add params to function array_left_shift()
    
    * Add basic word wrap with stub function
    
    * Add functions shift_array_by_delimiter() and shift_array_by_wrap()
    
    * Fix logic if impossible to wrap the word
    
    * Delete second param from function print_array()
    
    * Copy implementation of function find_word_wrap_position from word_wrap project
    
    * Add "-" to wrap output
    
    * Add big text to test
    
    * Add functions symbol_handler() and wrap_handler()

--- Ex_1.22/Ex_1.22.cpp
+++ Ex_1.22/Ex_1.22.cpp
@@ -3,11 +3,12 @@
 
 /* 
 program "folds" long input lines into shorter lines 
-Version_1: Program find the right position to shift words (after delimiter character)
+Version_2: Program wraps words partially
 NB! Program accept words for input no longer than MAXSIZE letters
 */
 
-# define MAXSIZE 50 //max size of the symbol array; it's also the length of line
+# define MAXSIZE 40 //max size of symbol array in the output string. Enter can have 11th position
+# define NO_WRAP -1 
 
 // ###########################################################################
 // global variables
@@ -15,20 +16,38 @@ NB! Program accept words for input no longer than MAXSIZE letters
 
 char c;
 char symb_arr[MAXSIZE + 1];
+char vowels_arr[] = "aeiou";
 int pos = 0; // position of the next symbol in input
-int delimiter_pos = -1; // position after the last delimiter
+int pos_after_delimiter = -1; // position after the last delimiter
 int print_to = -1; //the element to print array to
 char delimiter_arr[] = " /\\()\"\'-.,:;<>~!@#$%^&*|+=[]{}~\?│";
+int wrap_position = -1;
 
 // ###########################################################################
 // functions
 // ###########################################################################
 
-void print_array(int print_to, int is_new_line); //is_new_line: 1 - to print enter, 0 - not to print
-void find_limit_to_print();
+void symbol_handler();
+void wrap_handler();
+void print_array(int print_to); 
+void find_delimiter_limit_to_print();
 void init_array(int index_from, int index_to);
-void array_left_shift();
+void array_left_shift(int left_pos_to_shift);
 int is_delimiter(char c);
+void shift_array_by_delimiter();
+void shift_array_by_wrap();
+
+/* 
+function takes the word from the global array XXX and limit_position to wrap the word
+Returns the top right possible position of word wrap, otherwise -1
+Rules:
+1. Do not leave just one letter on the line (have at least 2 letters on the line)
+2. Each part should contain vowel letter (a, e, u, etc)
+3. Wrapping ends with vowel letter (e.g. hello -> he--llo)
+*/
+int find_word_wrap_position();
+int is_vowel(char symb);
+int find_vowel_position(int last_pos, int number);
 
 // ###########################################################################
 
@@ -40,42 +59,18 @@ int main()
 	{
 		if (pos < MAXSIZE)
 		{
-			
-			symb_arr[pos] = c;
-			pos++;
-			if (is_delimiter(c))
-			{
-				delimiter_pos = pos;
-			}
-			if (c == '\n')
-			{
-				print_array(pos, 0);
-				pos = 0;
-				delimiter_pos = -1;
-			}
+			symbol_handler();
 		}
-
 		else if (pos == MAXSIZE)
 		{
-			find_limit_to_print();
-			print_array(print_to, 1);
-			array_left_shift();
-			int last_symb = pos;
-			pos = (delimiter_pos > -1) ? (pos - delimiter_pos) : 0;
-			init_array(pos, last_symb);
-			if (c != '\n')
-			{
-				symb_arr[pos] = c;
-			}
-			pos++;
-			delimiter_pos = is_delimiter(c) ? pos : -1;
+			wrap_handler();
 		}
 	}
 
-	print_array(pos, 0);
-	
+	print_array(pos);
 	return EXIT_SUCCESS;
 }
+
 
 int is_delimiter(char c)
 {
@@ -90,24 +85,66 @@ int is_delimiter(char c)
 	return result;
 }
 
-void find_limit_to_print()
+
+void symbol_handler()
 {
-	print_to = (delimiter_pos > -1) ? delimiter_pos : pos;
+	symb_arr[pos] = c;
+	pos++;
+	if (is_delimiter(c))
+	{
+		pos_after_delimiter = pos;
+	}
+	if (c == '\n')
+	{
+		print_array(pos);
+		pos = 0;
+		pos_after_delimiter = -1;
+	}
 	return;
 }
 
-void print_array(int print_to, int is_new_line)
+
+void wrap_handler()
+{
+	find_delimiter_limit_to_print();
+	print_array(print_to);
+	shift_array_by_delimiter();
+
+	wrap_position = find_word_wrap_position();
+	if (wrap_position != NO_WRAP)
+	{
+		print_array(wrap_position);
+		putchar('-');
+		shift_array_by_wrap();
+	}
+	putchar('\n');
+
+	if (c != '\n')
+	{
+		symb_arr[pos] = c;
+	}
+	pos++;
+	pos_after_delimiter = is_delimiter(c) ? pos : -1;
+	return;
+}
+
+
+void find_delimiter_limit_to_print()
+{
+	print_to = (pos_after_delimiter > -1) ? pos_after_delimiter : pos;
+	return;
+}
+
+
+void print_array(int print_to)
 {
 	for (int i = 0; i < print_to; i++)
 	{
 		putchar(symb_arr[i]);
 	}
-	if (is_new_line == 1)
-	{
-		putchar('\n');
-	}
 	return;
 }
+
 
 void init_array(int index_from, int index_to)
 {
@@ -118,12 +155,111 @@ void init_array(int index_from, int index_to)
 	return;
 }
 
-void array_left_shift()
+
+void array_left_shift(int left_pos_to_shift)
 {
-	for (int i = delimiter_pos; i < pos; i++)
+	for (int i = left_pos_to_shift; i < pos; i++)
 	{
-		symb_arr[i - delimiter_pos] = symb_arr[i];
+		symb_arr[i - left_pos_to_shift] = symb_arr[i];
 
 	}
 	return;
+}
+
+
+void shift_array_by_delimiter()
+{
+	array_left_shift(pos_after_delimiter);
+	int last_symb = pos;
+	pos = (pos_after_delimiter > -1) ? (pos - pos_after_delimiter) : 0;
+	init_array(pos, last_symb);
+	return;
+}
+
+
+void shift_array_by_wrap()
+{
+	array_left_shift(wrap_position);
+	int last_symb = pos;
+	pos = pos - wrap_position;
+	init_array(pos, last_symb);
+	return;
+}
+
+
+int find_vowel_position(int last_pos, int number)
+{
+	int vowel_pos = -1;
+	int result = 0;
+
+	for (int n = 0; n < number; n++)
+	{
+		for (int i = last_pos; (i >= 0) && (result == 0); i--)
+		{
+			if (is_vowel(symb_arr[i]))
+			{
+				vowel_pos = last_pos;
+				result = 1;
+			}
+			last_pos--;
+		}
+		if ((last_pos < 0) && (result == 0))
+		{
+			vowel_pos = -1;
+		}
+		else
+		{
+			last_pos = vowel_pos - 1;
+			result = 0;
+		}
+	}
+	return vowel_pos;
+}
+
+
+int is_vowel(char symb)
+{
+	int result = 0;
+	for (int i = 0; (vowels_arr[i] != '\0') && (result == 0); i++)
+	{
+		if (symb == vowels_arr[i])
+		{
+			result = 1;
+		}
+
+	}
+	return result;
+}
+
+
+int find_word_wrap_position()
+{
+	int word_length = -1;
+	int vowel_position = -1;
+	int last_symb_position = -1;
+	int wrap_position = -1;
+
+	word_length = pos;
+	last_symb_position = word_length - 1;
+
+	if (word_length > 3)
+	{
+		vowel_position = find_vowel_position(last_symb_position, 2);
+
+		//printf("Vowel position = %d\n", vowel_position);
+		if ((vowel_position > 0) && ((last_symb_position - vowel_position) > 1))
+		{
+			wrap_position = vowel_position + 1;
+		}
+		else if ((last_symb_position - vowel_position) <= 1)
+		{
+			vowel_position = find_vowel_position(last_symb_position, 3);
+			if (vowel_position > 0)
+			{
+				wrap_position = vowel_position + 1;
+			}
+		}
+		//printf("Pos to wrap = %d\n", wrap_position);
+	}
+	return wrap_position;
 }
---
+++ Word_wrap/Word_wrap.cpp
@@ -0,0 +1,282 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+function takes the word from the global array XXX and limit_position to wrap the word
+Returns the top right possible position of word wrap, otherwise -1
+Rules:
+1. Do not leave just one letter on the line (have at least 2 letters on the line)
+2. Each part should contain vowel letter (a, e, u, etc)
+3. Wrapping ends with vowel letter (e.g. hello -> he--llo)
+*/
+
+# define MAXSIZE 20 //max size of the symbol array
+
+// ###########################################################################
+// functions
+// ###########################################################################
+
+int find_word_length();
+int is_vowel(char symb);
+int find_vowel_position(int last_pos, int number);
+int find_word_wrap_position(); // if -1 it is impossible to wrap
+int wrap_test(int expected_pos);
+void print_test_result(int test_result);
+
+/*
+int find_word_wrap_position_v2(const char* str);
+int wrap_test_v2(const char* str, int expected_pos);
+*/
+
+// ###########################################################################
+// global variables
+// ###########################################################################
+
+char word[MAXSIZE + 1];
+char vowels_arr[] = "aeiou";
+
+char test1[] = "hello"; //2
+char test2[] = "uncle"; //-1
+char test3[] = "bbba"; //-1
+char test4[] = "moloko"; //6
+char test5[] = "Jaromir"; //4
+char test6[] = "kitten"; //2
+char test7[] = "spaaain"; //5
+char test8[] = "bbbbb"; //-1
+char test9[] = "aaaa"; //2
+char test10[] = "sun"; //-1
+char test11[] = "ocean"; //-1
+
+// ###########################################################################
+
+int main()
+{
+	int test_result = -1;
+	int i = 0;
+
+	/*
+	int test1_result = wrap_test_v2("uncle", -1);
+	printf("Test1 %s \n", test1_result == 1 ? "passed" : "failed");
+	int test2_result = wrap_test_v2("hello", 2);
+	printf("Test2 %s \n", test1_result == 1 ? "passed" : "failed");
+	*/
+
+	//Add "hello" from array test1 to array word
+	for (i = 0; test1[i] != '\0'; i++)
+	{
+		word[i] = test1[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(2);
+	print_test_result(test_result);
+
+	//Add "uncle" from array test2 to array word
+	for (i = 0; test2[i] != '\0'; i++)
+	{
+		word[i] = test2[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(-1);
+	print_test_result(test_result);
+
+	//Add "bbba" from array test3 to array word
+	for (i = 0; test3[i] != '\0'; i++)
+	{
+		word[i] = test3[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(-1);
+	print_test_result(test_result);
+
+	//Add "moloko" from array test4 to array word
+	for (i = 0; test4[i] != '\0'; i++)
+	{
+		word[i] = test4[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(6);
+	print_test_result(test_result);
+
+	//Add "Jaromir" from array test5 to array word
+	for (i = 0; test5[i] != '\0'; i++)
+	{
+		word[i] = test5[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(4);
+	print_test_result(test_result);
+
+	//Add "kitten" from array test6 to array word
+	for (i = 0; test6[i] != '\0'; i++)
+	{
+		word[i] = test6[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(2);
+	print_test_result(test_result);
+
+	//Add "spaaain" from array test7 to array word
+	for (i = 0; test7[i] != '\0'; i++)
+	{
+		word[i] = test7[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(5);
+	print_test_result(test_result);
+
+	//Add "bbbbb" from array test8 to array word
+	for (i = 0; test8[i] != '\0'; i++)
+	{
+		word[i] = test8[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(-1);
+	print_test_result(test_result);
+
+	//Add "aaaa" from array test9 to array word
+	for (i = 0; test9[i] != '\0'; i++)
+	{
+		word[i] = test9[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(2);
+	print_test_result(test_result);
+
+	//Add "sun" from array test10 to array word
+	for (i = 0; test10[i] != '\0'; i++)
+	{
+		word[i] = test10[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(-1);
+	print_test_result(test_result);
+
+	//Add "ocean" from array test11 to array word
+	for (i = 0; test11[i] != '\0'; i++)
+	{
+		word[i] = test11[i];
+	}
+	word[i] = '\0';
+	test_result = wrap_test(3);
+	print_test_result(test_result);
+
+	return EXIT_SUCCESS;
+}
+
+/*
+int find_word_wrap_position_v2(const char* str)
+{
+	return -99;
+}
+
+int wrap_test_v2(const char* str, int expected_pos)
+{
+	int actual_pos = find_word_wrap_position_v2(str);
+	int result = (actual_pos == expected_pos) ? 1 : 0;
+	return result;
+}
+*/
+
+
+int find_word_length()
+{
+	int count = 0;
+	for (int i = 0; word[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return count;
+}
+
+
+int find_vowel_position(int last_pos, int number)
+{
+	int vowel_pos = -1;
+	int result = 0;
+
+	for (int n = 0; n < number; n++)
+	{
+		for (int i = last_pos; (i >= 0) && (result == 0); i--)
+		{
+			if (is_vowel(word[i]))
+			{
+				vowel_pos = last_pos;
+				result = 1;
+			}
+			// TO DO: if there is no vowels
+			last_pos--;
+		}
+		if ((last_pos < 0) && (result == 0))
+		{
+			vowel_pos = -1;
+		}
+		else
+		{
+			last_pos = vowel_pos - 1;
+			result = 0;
+		}
+	}
+	return vowel_pos;
+}
+
+
+int is_vowel(char symb)
+{
+	int result = 0;
+	for (int i = 0; (vowels_arr[i] != '\0') && (result == 0); i++)
+	{
+		if (symb == vowels_arr[i])
+		{
+			result = 1;
+		}
+
+	}
+	return result;
+}
+
+
+int wrap_test(int expected_pos)
+{
+	int actual_pos = find_word_wrap_position();
+	int result = (actual_pos == expected_pos) ? 1 : 0;
+	return result;
+}
+
+
+int find_word_wrap_position()
+{
+	int word_length = -1;
+	int vowel_position = -1;
+	int last_symb_position = -1;
+	int wrap_position = -1;
+
+	word_length = find_word_length();
+	last_symb_position = word_length - 1;
+
+	if (word_length > 3)
+	{
+		vowel_position = find_vowel_position(last_symb_position, 2);
+
+		//printf("Vowel position = %d\n", vowel_position);
+		if ((vowel_position > 0) && ((last_symb_position - vowel_position) > 1))
+		{
+			wrap_position = vowel_position + 1;
+		}
+		else if ((last_symb_position - vowel_position) <= 1)
+		{
+			vowel_position = find_vowel_position(last_symb_position, 3);
+			if (vowel_position > 0)
+			{
+				wrap_position = vowel_position + 1;
+			}
+		}
+		//printf("Pos to wrap = %d\n", wrap_position);
+	}
+	return wrap_position;
+}
+
+void print_test_result(int test_result)
+{
+	printf("Test %s:\t\t%s \n", word, test_result == 1 ? "PASSED" : "FAILED");
+	return;
+}
