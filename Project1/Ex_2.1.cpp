2dc7661a0571d514400145975e6c5f6cfae1883a
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2021-11-02 14:44:01 +0300

    2.1: Ranges of data types (#13)
    * Create Ex_2.1 with main template
    
    * Add values of signed, unsigned char
    
    * Add values for signed, unsigned int
    
    * Add values of signed/unsigned short int
    
    * Add values of signed,unsigned long int

---
+++ Project1/Ex_2.1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+//
+
+int main()
+{
+	printf("Ranges of data types from standart headers: \n\n");
+
+	printf("Signed char: min %d, max %d\n", SCHAR_MIN, SCHAR_MAX);
+	printf("Unsigned char: min %d, max %u\n", 0, UCHAR_MAX);
+
+	printf("Signed int: min %d, max %d\n", INT_MIN, INT_MAX);
+	printf("Unsigned int: min %d, max %u\n", 0, UINT_MAX);
+
+	printf("Signed short int: min %d, max %d\n", SHRT_MIN, SHRT_MAX);
+	printf("Unsigned short int: min %d, max %u\n", 0, USHRT_MAX);
+
+	printf("Signed long int: min %ld, max %ld\n", LONG_MIN, LONG_MAX);
+	printf("Unsigned long int: min %d, max %lu\n", 0, ULONG_MAX);
+
+
+	return EXIT_SUCCESS;
+}
+
