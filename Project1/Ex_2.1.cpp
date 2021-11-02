3197b76b9a83bef2ad463a572b9dfc118a1850ef
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-02 13:50:31 +0300

    Add values of signed, unsigned char

--- Project1/Ex_2.1.cpp
+++ Project1/Ex_2.1.cpp
@@ -7,7 +7,10 @@
 
 int main()
 {
-	
+	printf("Ranges of data types from standart headers: \n\n");
+
+	printf("Signed char: min %d, max %d\n", SCHAR_MIN, SCHAR_MAX);
+	printf("Unsigned char: min %d, max %d\n", 0, UCHAR_MAX);
 	return EXIT_SUCCESS;
 }
 
