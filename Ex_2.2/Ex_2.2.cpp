e293ba21c5e14ee3b038feaf40b081c8b26b2358
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-05 13:11:37 +0300

    Add loop equivalent withou operators && and ||

--- Ex_2.2/Ex_2.2.cpp
+++ Ex_2.2/Ex_2.2.cpp
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//
+/*
+Loop equivalent for
+
+for (i = 0; i < lim-1 && (ñ = getchar()) != EOF && ñ != '\n'; ++i)
+ s[i] = c;
+
+ without operators && and ||
+*/
+
+# define LIM 10
 
 int main()
 {
+	char c;
+	char s[LIM];
 
+	for (int i = 0; i < LIM - 1; i++)
+	{
+		if ((c = getchar()) != EOF)
+		{
+			if (c != '\n')
+			{
+				s[i] = c;
+			}
+		}
+
+	}
 
 	return EXIT_SUCCESS;
 }
+
+
