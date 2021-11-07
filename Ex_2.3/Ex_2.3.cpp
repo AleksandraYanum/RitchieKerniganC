4eccfe40eeb1a8c5453ea525bbbdaa71280de685
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-07 15:18:45 +0300

    Add txt file for testing

--- Ex_2.3/Ex_2.3.cpp
+++ Ex_2.3/Ex_2.3.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//
+// Program converts hexadecimal digits from string to integer value
+
+#define MAXSIZE 10
 
 int main()
 {
+	int htoi(char string[]);
+	char string[MAXSIZE];
 	
 	return EXIT_SUCCESS;
 }
