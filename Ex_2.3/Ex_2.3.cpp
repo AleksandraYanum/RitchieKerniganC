53ce0ad77871948df4c541d9e39608b0f3747231
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-07 17:05:45 +0300

    Add initial version of convertation dex to int from string

--- Ex_2.3/Ex_2.3.cpp
+++ Ex_2.3/Ex_2.3.cpp
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 // Program converts hexadecimal digits from string to integer value
 
-#define MAXSIZE 10
+#define MAXSIZE 100
+
 
 int main()
 {
-	int htoi(char string[]);
-	char string[MAXSIZE];
+
+	char string[MAXSIZE] = "123AB";
+	char c; 
+	int hex = 0;
+	long int number = 0;
+
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		if (isdigit(string[i]))
+		{
+			hex = string[i] - '0';
+		}
+		if (string[i] >= 'A' && string[i] <= 'F')
+		{
+			hex = string[i] - 'A' + 10;
+		}
+		if (string[i] >= 'a' && string[i] <= 'f')
+		{
+			hex = string[i] - 'a' + 10;
+		}
+		number = 16 * number + hex;
+	}
+
+	printf("Integer equivalent is %ld", number);
 	
 	return EXIT_SUCCESS;
 }
