8aa1f8e3befac008222e48c19ef22cf2ff9fc701
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-08 01:10:29 +0300

    Add function is_new_hex_digit(0

--- Ex_2.3/Ex_2.3.cpp
+++ Ex_2.3/Ex_2.3.cpp
@@ -5,6 +5,8 @@
 // Program converts hexadecimal digits from string to integer value
 
 #define MAXSIZE 100
+#define IN 1
+#define OUT 0
 
 // ###########################################################################
 // functions
@@ -12,15 +14,23 @@
 
 long int htoi(char s[]);
 int is_hexadecimal(char c);
+int is_new_hex_digit(char s[]);
+
+// ###########################################################################
+// global variables
+// ###########################################################################
+
+int pos = -1;
 
 // ###########################################################################
 
 int main()
 {
 	long int n = 0;
-	char string[MAXSIZE] = "123AB---";
+	char string[MAXSIZE] = "0X1F--";
 	
 	n = htoi(string);
+
 	printf("Integer equivalent is %ld", n);
 	
 	return EXIT_SUCCESS;
@@ -32,21 +42,24 @@ long int htoi(char s[])
 	int hex = 0;
 	long int number = 0;
 
-	for (int i = 0; is_hexadecimal(s[i]); i++)
+	if (is_new_hex_digit(s))
 	{
-		if (isdigit(s[i]))
+		for (int i = pos; is_hexadecimal(s[i]); i++)
 		{
-			hex = s[i] - '0';
+			if (isdigit(s[i]))
+			{
+				hex = s[i] - '0';
+			}
+			if (s[i] >= 'A' && s[i] <= 'F')
+			{
+				hex = s[i] - 'A' + 10;
+			}
+			if (s[i] >= 'a' && s[i] <= 'f')
+			{
+				hex = s[i] - 'a' + 10;
+			}
+			number = 16 * number + hex;
 		}
-		if (s[i] >= 'A' && s[i] <= 'F')
-		{
-			hex = s[i] - 'A' + 10;
-		}
-		if (s[i] >= 'a' && s[i] <= 'f')
-		{
-			hex = s[i] - 'a' + 10;
-		}
-		number = 16 * number + hex;
 	}
 	return number;
 }
@@ -60,6 +73,23 @@ int is_hexadecimal(char c)
 		(c >= 'a' && c <= 'f'))
 	{
 			result = 1;
+	}
+	return result;
+}
+
+int is_new_hex_digit(char s[])
+{
+	char prev_symb = 0;
+	int result = 0;
+	
+	for (int i = 0; (s[i] != '\0') && (result == 0); i++)
+	{
+		if ((s[i] == 'x' || s[i] == 'X') && (prev_symb == '0'))
+		{
+			result = 1;
+			pos = i + 1;
+		}
+		prev_symb = s[i];
 	}
 	return result;
 }
