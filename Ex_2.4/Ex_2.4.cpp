ac8da0d055cb48eb15d76e2ce4510aef2b5fd104
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-08 15:12:30 +0300

    Add stub function squeeze()

--- Ex_2.4/Ex_2.4.cpp
+++ Ex_2.4/Ex_2.4.cpp
@@ -3,8 +3,28 @@
 
 //Program deletes each character in s1 that matches any character in the string s2
 
+#define MAXSIZE 100
+
+// ###########################################################################
+// functions
+// ###########################################################################
+
+void squeeze(char s1[], char s2[]);
+
+// ###########################################################################
+
 int main()
 {
-	
+	char main_string[MAXSIZE];
+	char string_to_delete[MAXSIZE];
+
+	squeeze(main_string, string_to_delete);
+
 	return EXIT_SUCCESS;
+}
+
+
+void squeeze(char s1[], char s2[])
+{
+	return;
 }
