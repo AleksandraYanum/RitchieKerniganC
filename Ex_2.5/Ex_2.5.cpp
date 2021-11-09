0f6950212e4932c5a8f20f5e893d80d86369f3aa
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-09 16:04:47 +0300

    Add stub function any()

--- Ex_2.5/Ex_2.5.cpp
+++ Ex_2.5/Ex_2.5.cpp
@@ -13,6 +13,7 @@ or-1 if s1 contains no characters from s2
 // ###########################################################################
 
 int getline(char s[], int lim);
+int any(const char source[], const char pattern[]);
 
 // ###########################################################################
 
@@ -26,6 +27,8 @@ int main()
 
 	printf("\nInput chars to find in main string:\n");
 	getline(string_to_find, MAXSIZE);
+
+	any(main_string, string_to_find);
 
 	return EXIT_SUCCESS;
 }
@@ -44,4 +47,11 @@ int getline(char s[], int lim)
 	}
 	s[end_of_line] = '\0';
 	return i;
+}
+
+
+int any(const char source[], const char pattern[])
+{
+
+	return 0;
 }
