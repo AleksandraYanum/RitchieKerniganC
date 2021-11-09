986d5c7801dd3073ac7e588c2f2fd0cae20bc38d
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-09 16:02:56 +0300

    Add function getline()

--- Ex_2.5/Ex_2.5.cpp
+++ Ex_2.5/Ex_2.5.cpp
@@ -6,8 +6,42 @@ Program returns the first location in the string s1 where any character from the
 or-1 if s1 contains no characters from s2
 */
 
+#define MAXSIZE 101
+
+// ###########################################################################
+// functions
+// ###########################################################################
+
+int getline(char s[], int lim);
+
+// ###########################################################################
+
 int main()
 {
-	
+	char main_string[MAXSIZE];
+	char string_to_find[MAXSIZE];
+
+	printf("Input main string no longer than %d symbols:\n", (MAXSIZE - 1));
+	getline(main_string, MAXSIZE);
+
+	printf("\nInput chars to find in main string:\n");
+	getline(string_to_find, MAXSIZE);
+
 	return EXIT_SUCCESS;
+}
+
+
+int getline(char s[], int lim)
+{
+	int i;
+	int end_of_line = 0;
+	int c;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+	{
+		s[i] = c;
+		end_of_line = i + 1;
+	}
+	s[end_of_line] = '\0';
+	return i;
 }
