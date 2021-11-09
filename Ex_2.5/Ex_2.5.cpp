1b64532404bf268f2893cb34a6d344f16a5fac50
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-09 15:57:13 +0300

    Add Ex_2.5 with main template

---
+++ Ex_2.5/Ex_2.5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Program returns the first location in the string s1 where any character from the string s2 occurs, 
+or-1 if s1 contains no characters from s2
+*/
+
+int main()
+{
+	
+	return EXIT_SUCCESS;
+}
