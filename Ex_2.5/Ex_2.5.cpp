27899f25595a4eeb60f6d10622522e18857db324
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2021-11-10 10:52:36 +0300

    2.5: Any function (#23)
    * Add Ex_2.5 with main template
    
    * Add function getline()
    
    * Add stub function any()
    
    * Add unit test template
    
    * Add all test cases
    
    * Delete obsolete code
    
    * Implement any()

---
+++ Ex_2.5/Ex_2.5.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Program returns the first location in the string s1 where any character from the string s2 occurs,
+or-1 if s1 contains no characters from s2
+*/
+
+
+//###########################################################################
+// functions
+// ###########################################################################
+
+int any(const char source[], const char pattern[]);
+int any_test(const char source[], const char pattern[], int expected_pos);
+
+// ###########################################################################
+
+
+int main()
+{
+	int test_result = 0;
+	
+	test_result = any_test("aleksandra yanum", "knky", 3);
+	printf("Test1 %s \n", test_result == 1 ? "passed" : "failed");
+	
+	test_result = any_test("", "knky", -1);
+	printf("Test2 %s \n", test_result == 1 ? "passed" : "failed");
+	
+	test_result = any_test("vlad yanum", "", -1);
+	printf("Test3 %s \n", test_result == 1 ? "passed" : "failed");
+
+	test_result = any_test("", "", -1);
+	printf("Test4 %s \n", test_result == 1 ? "passed" : "failed");
+
+	test_result = any_test("qwetyqwerty", "rt", 3);
+	printf("Test5 %s \n", test_result == 1 ? "passed" : "failed");
+
+	test_result = any_test("aleksandra", "bmw", -1);
+	printf("Test6 %s \n", test_result == 1 ? "passed" : "failed");
+	
+	return EXIT_SUCCESS;
+}
+
+
+int any(const char source[], const char pattern[])
+{
+	int i = 0;
+	int pos = -1;
+
+	while ((source[i] != '\0') && (pos == -1))
+	{
+		int j = 0;
+		while ((pattern[j] != '\0') && (pos == -1))
+		{
+			if (source[i] != pattern[j])
+			{
+				j++;
+			}
+			else
+			{
+				pos = i;
+			}
+		}
+		i++;
+	}
+	return pos;
+}
+
+
+int any_test(const char source[], const char pattern[], int expected_pos)
+{
+	int actual_pos = any(source, pattern);
+	int result = (actual_pos == expected_pos);
+	return result;
+}
