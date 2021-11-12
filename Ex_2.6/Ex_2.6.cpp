78e39892ac63af4b3338b9395073baa3ddd82194
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-12 11:13:01 +0300

    Mask part_before_pos in source_number

--- Ex_2.6/Ex_2.6.cpp
+++ Ex_2.6/Ex_2.6.cpp
@@ -33,8 +33,14 @@ int main()
 
 unsigned int setbits(unsigned int target_number, int pos_from, int bit_amount, unsigned int source_number)
 {
-	//Fetch rightmost bit_amount bits from sourse_number and move it to the pos_from in target_number
-	source_number = source_number & ~(~ (unsigned) 0 << bit_amount) << (pos_from + 1 - bit_amount);
+	unsigned int part_before_pos = 0;
+
+	//Mask rightmost bit_amount bits from sourse_number and move it to the pos_from in target_number
+	source_number = source_number & ~(~ (unsigned) 0 << bit_amount) << (pos_from + 1 - bit_amount); 
+
+	//Target_number can be represented as: part_before_pos + pos_from + part_after_pos
+	//Mask part_before_pos
+	part_before_pos = source_number & (~(unsigned)0 << (pos_from + 1));
 
 	return 0;
 }
