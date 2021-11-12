1f2e1d3c42f70cdeb127b95368415ea847245eb5
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-12 11:18:20 +0300

    Bug fix: replace source_number with target_number

--- Ex_2.6/Ex_2.6.cpp
+++ Ex_2.6/Ex_2.6.cpp
@@ -41,10 +41,12 @@ unsigned int setbits(unsigned int target_number, int pos_from, int bit_amount, u
 
 	//Target_number can be represented as: part_before_pos + pos_from + part_after_pos
 	//Mask part_before_pos
-	part_before_pos = source_number & (~(unsigned) 0 << (pos_from + 1));
+	part_before_pos = target_number & (~(unsigned) 0 << (pos_from + 1));
 
 	//Mask part_after_pos
-	part_after_pos = source_number & ~(~(unsigned) 0 << (pos_from + 1 - bit_amount));
+	part_after_pos = target_number & ~(~(unsigned) 0 << (pos_from + 1 - bit_amount));
+
+
 
 	return 0;
 }
