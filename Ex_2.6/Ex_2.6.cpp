6f8fb4e712909804ed8d36f40603608f056a8332
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-12 11:33:57 +0300

    Set new target_number

--- Ex_2.6/Ex_2.6.cpp
+++ Ex_2.6/Ex_2.6.cpp
@@ -23,30 +23,30 @@ int main()
 	unsigned int source_number = 0b11000111;
 	unsigned int result_number = 0;
 
-	setbits(target_number, pos_from, bit_amount, source_number);
-
-	
-	
+	result_number = setbits(target_number, pos_from, bit_amount, source_number);
+	//printf("%d", result_number);
 	return EXIT_SUCCESS;
 }
 
 
 unsigned int setbits(unsigned int target_number, int pos_from, int bit_amount, unsigned int source_number)
 {
-	unsigned int part_before_pos = 0;
-	unsigned int part_after_pos = 0;
+	unsigned int part_before_pos = 0b0;
+	unsigned int part_after_pos = 0b0;
 
 	//Mask rightmost bit_amount bits from sourse_number and move it to the pos_from in target_number
-	source_number = source_number & ~(~ (unsigned) 0 << bit_amount) << (pos_from + 1 - bit_amount); 
+	source_number = source_number & ~(~(unsigned)0 << bit_amount) << (pos_from + 1 - bit_amount);
+	//printf("%d", source_number);
 
 	//Target_number can be represented as: part_before_pos + pos_from + part_after_pos
 	//Mask part_before_pos
-	part_before_pos = target_number & (~(unsigned) 0 << (pos_from + 1));
+	part_before_pos = target_number & (~(unsigned)0 << (pos_from + 1));
 
 	//Mask part_after_pos
-	part_after_pos = target_number & ~(~(unsigned) 0 << (pos_from + 1 - bit_amount));
+	part_after_pos = target_number & ~(~(unsigned)0 << (pos_from + 1 - bit_amount));
 
+	//Set to one in target_number bits that are set to one in part_before_pos, part_after_pos and source_number
+	target_number = part_before_pos | part_after_pos | source_number;
 
-
-	return 0;
+	return target_number;
 }
