112aeee24c287ed803077fdb18d171c8d0451256
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-13 16:27:26 +0300

    Add stub function rightrot()

--- Ex_2.8/Ex_2.8.cpp
+++ Ex_2.8/Ex_2.8.cpp
@@ -3,8 +3,21 @@
 
 //Program returns the value of the int target_number rotated to the right by bit_amount bit positions
 
+//###########################################################################
+// functions
+// ###########################################################################
+
+unsigned int rightrot(unsigned int target_number, int bit_amount);
+
 int main()
 {
 	
 	return EXIT_SUCCESS;
+}
+
+
+unsigned int rightrot(unsigned int target_number, int bit_amount)
+{
+
+	return 0;
 }
