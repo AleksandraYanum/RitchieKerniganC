0af6b691a8eddc1f4b39a36de52f2a2a5c867591
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-13 16:40:33 +0300

    Add unit tests

--- Ex_2.8/Ex_2.8.cpp
+++ Ex_2.8/Ex_2.8.cpp
@@ -8,10 +8,21 @@
 // ###########################################################################
 
 unsigned int rightrot(unsigned int target_number, int bit_amount);
+unsigned int rightrot_test(unsigned int target_number, int bit_amount, unsigned int expected);
 
 int main()
 {
-	
+	int test_result = 0;
+
+	test_result = rightrot_test(0b00000000000000000000000011011001, 3, 0b00100000000000000000000000011011);
+	printf("Test1 %s \n", test_result == 1 ? "passed" : "failed");
+
+	test_result = rightrot_test(0b101, 32, 0b101);
+	printf("Test2 %s \n", test_result == 1 ? "passed" : "failed");
+
+	test_result = rightrot_test(1, 34, 1073741824);
+	printf("Test3 %s \n", test_result == 1 ? "passed" : "failed");
+
 	return EXIT_SUCCESS;
 }
 
@@ -20,4 +31,12 @@ unsigned int rightrot(unsigned int target_number, int bit_amount)
 {
 
 	return 0;
+}
+
+
+unsigned int rightrot_test(unsigned int target_number, int bit_amount, unsigned int expected)
+{
+	unsigned int actual = rightrot(target_number, bit_amount);
+	unsigned int result = (actual == expected);
+	return result;
 }
