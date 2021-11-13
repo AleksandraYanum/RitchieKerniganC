2e88ebdf407681cb0b86038da871ca414c6a16c5
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-13 12:32:29 +0300

    Add Ex_2.7 with main template

---
+++ Ex_2.7/Ex_2.7.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Program returns target_number with the bit_amount bits that begin at position pos_from inverted 
+(i.e., 1 changed into 0 and vice versa), leaving the others unchanged. 
+*/
+
+int main()
+{
+	
+	return EXIT_SUCCESS;
+}
