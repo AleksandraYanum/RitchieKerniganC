c2ae80e52d18aad5a9d9e1ef32d97826dae8ae5d
AleksandraYanum <79569872+AleksandraYanum@users.noreply.github.com>
2021-11-13 17:36:11 +0300

    2.8: Rightrot function (#26)
    * Add Ex_2.8 with main template
    
    * Add stub function rightrot()
    
    * Add unit tests
    
    * Implement rightrot()
    
    * Improve performance of rightrot()
    
    * Fix code view
    
    * Rightrot() refactoring
    
    * Fix var's scope

---
+++ Ex_2.8/Ex_2.8.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Program returns the value of the int target_number rotated to the right by bit_amount bit positions
+
+//###########################################################################
+// functions
+// ###########################################################################
+
+unsigned int rightrot(unsigned int number, int bit_amount);
+unsigned int rightrot_test(unsigned int number, int bit_amount, unsigned int expected);
+
+// ###########################################################################
+
+int main()
+{
+	int test_result = 0;
+
+	test_result = rightrot_test(0b00000000000000000000000011011001, 3, 0b00100000000000000000000000011011);
+	printf("Test1 %s \n", test_result == 1 ? "passed" : "failed");
+
+	test_result = rightrot_test(0b101, 32, 0b101);
+	printf("Test2 %s \n", test_result == 1 ? "passed" : "failed");
+
+	test_result = rightrot_test(1, 34, 1073741824);
+	printf("Test3 %s \n", test_result == 1 ? "passed" : "failed");
+
+	return EXIT_SUCCESS;
+}
+
+
+unsigned int rightrot(unsigned int number, int bit_amount)
+{
+	int type_size = sizeof(number) * 8;
+	bit_amount = bit_amount % type_size;
+
+	if (bit_amount > 0)
+	{
+		unsigned int number_to_left = number << type_size - bit_amount;
+		unsigned int number_to_right = number >> bit_amount;
+		number = number_to_left | number_to_right;
+	}
+	return number;
+}
+
+
+unsigned int rightrot_test(unsigned int number, int bit_amount, unsigned int expected)
+{
+	unsigned int actual = rightrot(number, bit_amount);
+	unsigned int result = (actual == expected);
+	return result;
+}
