0a26a073ca9c47c60d5fafa3100e32f0d12e7bea
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-13 16:20:58 +0300

    Add Ex_2.8 with main template

---
+++ Ex_2.8/Ex_2.8.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Program returns the value of the int target_number rotated to the right by bit_amount bit positions
+
+int main()
+{
+	
+	return EXIT_SUCCESS;
+}
