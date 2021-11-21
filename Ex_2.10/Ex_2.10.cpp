e47639d1480dc703c75632ca4862d1e3b23dc9e6
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-21 15:21:42 +0300

    Add Ex_2.10 with main template

---
+++ Ex_2.10/Ex_2.10.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Program converts upper case lettres to lower case (with a conditional expression instead of if-else)
+
+int main()
+{
+	
+	return EXIT_SUCCESS;
+}
