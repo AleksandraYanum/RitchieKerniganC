d3a10f40f5a5b18e6f7a189ade5442729bfb434e
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-11-21 15:25:11 +0300

    Add stub function lower()

--- Ex_2.10/Ex_2.10.cpp
+++ Ex_2.10/Ex_2.10.cpp
@@ -3,8 +3,23 @@
 
 //Program converts upper case lettres to lower case (with a conditional expression instead of if-else)
 
+//###########################################################################
+// functions
+// ###########################################################################
+
+int lower(int c);
+
+// ###########################################################################
+
 int main()
 {
 	
 	return EXIT_SUCCESS;
 }
+
+int lower(int c)
+{
+
+	return 0;
+}
+
