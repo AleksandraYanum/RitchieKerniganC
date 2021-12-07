be6a5a9f17b7ec948458ec0c5a8283f411a6957c
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-12-07 14:26:57 +0300

    Add 3.1(v1) with main_template

---
+++ Ex_3.1/Ex_3.1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//binsearch: find 'number' in array and return its pos else -1
+
+int binsearch(int number, int array[], int amount);
+
+
+int main()
+{
+	int pos = 0;
+	int number = 6;
+	int array[] = { 1, 3, 6, 8, 10, 12, 15, 14, 15, 20, 21 };
+	int amount = 11;
+
+	pos = binsearch(number, array, amount - 1);
+
+	return EXIT_SUCCESS;
+}
+
+
+int binsearch(int number, int array[], int amount)
+{
+	return 0;
+}
