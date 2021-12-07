56fd990b081b30aeb2290733465c1e765480345d
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-12-07 14:29:44 +0300

    Add stub function binsearch()

--- Ex_3.1/Ex_3.1.cpp
+++ Ex_3.1/Ex_3.1.cpp
@@ -13,7 +13,7 @@ int main()
 	int array[] = { 1, 3, 6, 8, 10, 12, 15, 14, 15, 20, 21 };
 	int amount = 11;
 
-	pos = binsearch(number, array, amount - 1);
+	pos = binsearch(number, array, amount);
 
 	return EXIT_SUCCESS;
 }
@@ -21,5 +21,9 @@ int main()
 
 int binsearch(int number, int array[], int amount)
 {
+	int low_pos = 0;
+	int high_pos = amount - 1;
+	int mid = 0;
+
 	return 0;
 }
