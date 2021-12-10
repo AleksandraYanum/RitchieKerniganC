e77f76ce9e633c0f76dacea1011d6877e14d5bfd
Aleksandra Yanum <aleksandra.gladysh@gmail.com>
2021-12-10 15:15:53 +0300

    Implement binsearch() version2 with one check in the loop

--- Ex_3.1/Ex_3.1.cpp
+++ Ex_3.1/Ex_3.1.cpp
@@ -12,7 +12,7 @@ int binsearch(int number, int array[], int amount);
 int main()
 {
 	int pos = 0;
-	int number = 6;
+	int number = 21;
 	int array[SIZE] = { 1, 3, 6, 8, 10, 12, 15, 16, 18, 20, 21 };
 
 	pos = binsearch(number, array, SIZE);
@@ -29,22 +29,19 @@ int binsearch(int number, int array[], int amount)
 	int mid = 0;
 	int number_pos = -1;
 
-	while ((low_pos <= high_pos) && (number_pos == -1))
+	while ((low_pos <= high_pos) && (number != array[mid]))
 	{
 		mid = (low_pos + high_pos) / 2;
 		if (number < array[mid])
 		{
 			high_pos = mid - 1;
 		}
-		else if (number > array[mid])
+		else 
 		{
 			low_pos = mid + 1;
 		}
-		else
-		{
-			number_pos = mid;
-		}
 	}
 
+	number_pos = (number == array[mid]) ? mid : -1;
 	return number_pos;
 }
